f788366483e9e394f073511d0ab48fd14845f799
Leonid Samoshenko <samoshenkoleonid@yandex.ru>
2024-10-22 09:14:22 +0300

    change

--- l5/f2.py
+++ l5/f2.py
@@ -5,6 +5,7 @@ print('welcome to variant 22')
 
 l = math.log(2)
 t = 0
-while 2 * math.e ** (-1 * t * l / 8):
+while 2 * math.e ** (-1 * t * l / 8) >= 0.15:
     t += 5
 print ('required number of days: ' + str(t))
+print(math.e ** (-1 * t * l / 8) == math.exp(-1 * t * l / 8))
