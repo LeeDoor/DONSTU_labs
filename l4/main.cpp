d20660d762ea61f91048b2a757bd5786bba0f265
leedoor <samoshenkoleonid@yandex.ru>
2025-03-18 09:49:46 +0300

    razrabi dauni

--- l4/main.cpp
+++ l4/main.cpp
@@ -20,21 +20,19 @@ int main() {
     wchar_t buffer[BUF_SIZE];
     wprintf(L"Enter line: ");
     fgetws(buffer, BUF_SIZE, stdin);
-
-    wprintf(L"Initial line: %ls\n", buffer);
+    wprintf(L"Initial line: %ls", buffer); // NEW-LINE terminated 
     size_t cyr = 0, lat = 0, strs = 0;
-    for (size_t i = 0; buffer[i] != L'\0'; ++i, ++strs) {
+    for (size_t i = 0; buffer[i] != L'\n'; ++i, ++strs) {
         cyr += is_cyrillic(buffer[i]);
         lat += is_latin(buffer[i]);
     }
     wprintf(L"cyrillic letters: %zd\n"
             L"latin letters: %zd\n",
             cyr, lat);
-    printf("%zd is size\n", strs);
-    for(size_t i = 0; i < strs; ++i) {
+    for(size_t i = 0; i < strs - 1; ++i) {
         wchar_t swap = buffer[i];
-        buffer[i] = buffer[(i + 1) % strs];
-        buffer[(i + 1) % strs] = swap;
+        buffer[i] = buffer[i + 1];
+        buffer[i + 1] = swap;
     }
     wprintf(L"Edited line: %ls\n", buffer);
     return 0;
